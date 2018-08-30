c4a2f1c7a179327f27d95bf1c52265de6edf4c8e
Sarah <eaaskh@students.eaaa.dk>
2018-08-30 12:13:20 +0200

    Samling, ændring af css
    Samling af Home, your fridge, load, shopping list, modal-box.
    
    Ændring af layout i CSS til your fridge.
    
    Det nye er taget med ind i style.css - der indeholder style til alt

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName("myBtn").addEventListener("click", function(){
+    document.getElementsByClassName(".shopping").innerHTML = "Hello World";
+
+});
+
+document.getElementById("myBtn").addEventListener("click", function(){
+    document.getElementById("demo").innerHTML = "Hello World";
+});
+
