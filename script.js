d2c2ee8ef9f6cc833cd593ff8b75708052fe2399
Sarah <eaaskh@students.eaaa.dk>
2018-08-30 15:51:42 +0200

    Forsøg på god java

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-document.getElementsByClassName("myBtn").addEventListener("click", function(){
-    document.getElementsByClassName(".shopping").innerHTML = "Hello World";
+/* Frontpage buttons */
+var a = document.getElementById("see-fridge");
+var b = document.getElementById("see-items");
+var c = document.getElementById("see-shopping");
 
-});
+function seeFridge() {
+    document.getElementById("your-fridge").style.display = "grid";
+    document.getElementById("home").style.display = "none";
+}
 
-document.getElementById("myBtn").addEventListener("click", function(){
-    document.getElementById("demo").innerHTML = "Hello World";
-});
+function seeItems() {
+    document.getElementById("add-food").style.display = "block";
+    document.getElementById("home").style.display = "none";
+}
 
+function seeShopping() {
+    document.getElementById("shopping-list").style.display = "grid";
+    document.getElementById("home").style.display = "none";
+}
+
+a.addEventListener('click', seeFridge, true);
+b.addEventListener('click', seeItems, true);
+c.addEventListener('click', seeShopping, true);
+
+/* Back buttons  */
+var tilbage = document.getElementById("back");
+var tilbageEt = document.getElementById("backEt");
+var tilbageTo = document.getElementById("backTo");
+
+function goBack() {
+    document.getElementById("your-fridge").style.display = "none";
+    document.getElementById("home").style.display = "grid";
+}
+
+function goBackEt() {
+    document.getElementById("add-food").style.display = "none";
+    document.getElementById("home").style.display = "grid";
+}
+
+function goBackTo() {
+    document.getElementById("shopping-list").style.display = "none";
+    document.getElementById("home").style.display = "grid";
+}
+
+tilbage.addEventListener('click', goBack, true);
+tilbageEt.addEventListener('click', goBackEt, true);
+tilbageTo.addEventListener('click', goBackTo, true);
+
+var d = document.getElementById("go-shopping");
+var e = document.getElementById("go-fridge");
+
+function goShopping (){
+    document.getElementById("your-fridge").style.display = "none";
+    document.getElementById("shopping-list").style.display = "grid";
+}
+
+function goFridge (){
+    document.getElementById("shopping-list").style.display = "none";
+    document.getElementById("your-fridge").style.display = "grid";
+    
+}
+
+d.addEventListener('click', goShopping, true);
+e.addEventListener('click', goFridge, true);
