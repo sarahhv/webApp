9df9700c4830ce6f4f15a80ce353b1b672d62595
Sarah <eaaskh@students.eaaa.dk>
2018-09-05 15:09:10 +0200

    s changes

--- jsonWorks/saveJson.php
+++ jsonWorks/saveJson.php
@@ -1,6 +1,13 @@
 <?php
-    $jsonStreng="{ \"food\":".$_POST["jsonData"]."}";
+    $jsonStreng="{ \"fridge\":".$_POST["jsonData"]."}";
     $file=fopen("webApp.json","w+") or die("Systemfejl: Fil kan ikke åbnes!!");
+    fputs($file,$jsonStreng) or die ("Systemfejl: Data kan ikke skrives til fil!!");
+    fclose($file);
+?>
+
+<?php
+    $jsonStreng="{ \"shopping\":".$_POST["jsonData"]."}";
+    $file=fopen("shopping.json","w+") or die("Systemfejl: Fil kan ikke åbnes!!");
     fputs($file,$jsonStreng) or die ("Systemfejl: Data kan ikke skrives til fil!!");
     fclose($file);
 ?>
